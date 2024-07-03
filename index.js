646a4cc55bd55d13bea45f22afed5a0321446c21
mohidChaudhry <124250910+mohidChaudhry@users.noreply.github.com>
2024-07-03 18:25:37 +0100

    index model import

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const User = require('./models/user.model.js');
 const app = express()
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
 });
 
 app.get('/', (req, res)=>{
-    res.send("hello from Node api servr");
+    res.send("hello from Node api server");
 });
+
+app.post('/api/users', (req, res) =>{
+    try{
+
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+
+});
+
 
 mongoose.connect("mongodb+srv://mohidjavedch:FLqtloOx6VpPxSlR@backenddb.earnplx.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
 .then(()=>{
